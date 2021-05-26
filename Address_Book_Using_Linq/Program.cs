4d6a179bd4ad9d40b2b2e91152808ce681142eb3
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-05-26 09:24:09 +0530

    initial commit

---
+++ Address_Book_Using_Linq/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book_Using_Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------WELCOME TO THE ADDRESSBOOK LINQ------------");
+
+            //Creating DataTable for addressbook problem UC1
+            DataTable addressbookcreatetable = new DataTable();
+        }
+    }
+}
