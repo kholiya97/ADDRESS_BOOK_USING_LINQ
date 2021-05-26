b89559412d076d77fe61a28c0083cbc0bfc2bfac
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-05-26 10:10:06 +0530

    Added file of UC2_CREATE_ADDRESSBOOK_TABLE_AND_ATTRIBUTES

--- Address_Book_Using_Linq/Program.cs
+++ Address_Book_Using_Linq/Program.cs
@@ -7,14 +7,36 @@ using System.Threading.Tasks;
 
 namespace Address_Book_Using_Linq
 {
+    class AddressBookDataTable
+    {
+        //Creating DataTable for addressbook problem UC1
+        DataTable dataTable = new DataTable();
+
+        /// Create the Address Book table and add attributes.
+
+        public void createAddressBookTable()
+        {
+            dataTable.Columns.Add("FirstName", typeof(string));
+            dataTable.Columns.Add("LastName", typeof(string));
+            dataTable.Columns.Add("Address", typeof(string));
+            dataTable.Columns.Add("City", typeof(string));
+            dataTable.Columns.Add("State", typeof(string));
+            dataTable.Columns.Add("ZipCode", typeof(int));
+            dataTable.Columns.Add("PhoneNumber", typeof(long));
+            dataTable.Columns.Add("Email", typeof(string));
+
+        }
+
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("-----------WELCOME TO THE ADDRESSBOOK LINQ------------");
-
-            //Creating DataTable for addressbook problem UC1
-            DataTable addressbookcreatetable = new DataTable();
+            Console.WriteLine("***************Welcome To AddressBook Using Linq*************");
+            AddressBookDataTable addressBookDataTable = new AddressBookDataTable();
+            addressBookDataTable.createAddressBookTable();
+            Console.Read();
         }
     }
 }
